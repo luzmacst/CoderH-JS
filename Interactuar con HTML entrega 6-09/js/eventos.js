9dfdd6c784b5c4c3319078b33ea6924ff9ab3c74
luzmacst <101640006+luzmacst@users.noreply.github.com>
2022-09-05 21:11:18 -0300

    entrega 6 de septiembre

---
+++ Interactuar con HTML entrega 6-09/js/eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    M.AutoInit();
+    });
+
+    // const $linkform = document.getElementsByClassName("link-form");
+
+    // /* $linkform.setAttribute("target", _blank); */
+    // console.log($linkform);
